8749eb3535480f9300adcce13177f270ffc61138
kang <dkemfjqek@gmail.com>
2022-03-04 00:18:47 +0900

    질문 상세 스타일링

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,7 +1,29 @@
 <template>
     <div>
-        <p>{{ itemInfo.title }}</p>
-        <p>{{ itemInfo.user }}</p>
+        <section>
+            <!-- 질문 상세 정보 -->
+            <div>
+                <div class="user-container">
+                    <div>
+                        <i class="fa-solid fa-user-secret"></i>
+                    </div>
+                    <div class="user-description">
+                        <router-link :to="`/user/${itemInfo.user}`">
+                            {{ itemInfo.user }}
+                        </router-link>
+                        <div class="time">
+                            {{ itemInfo.time_ago }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <h2>{{ itemInfo.title }}</h2>
+        </section>
+        <section>
+            <!-- 질문 댓글 -->
+            <div v-html="itemInfo.content"/> <!-- {{ itemInfo.content }} -->
+        </section>
     </div>
 </template>
 
@@ -19,6 +41,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.user-container {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+}
+.fa-user-secret{
+    font-size: 2.5rem;
+}
+.user-description {
+    padding-left: 8px;
+}
+.time {
+    font-size: 0.7rem;
+}
 </style>
